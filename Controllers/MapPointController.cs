658c973d5d4ebb0c1f7d548b91cbaba1bfe34f39
kd8itx <mweidner@gmail.com>
2024-09-29 16:29:29 -0400

    Initial commit of MinecraftTrackerApp

---
+++ Controllers/MapPointController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinecraftTrackerApp.DA.MapPoint;
+using MinecraftTrackerObjects;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MinecraftTrackerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapPointController : ControllerBase
+    {
+        MapPointDA mpDA = new MapPointDA("Server=minecraft.cpesk00qmvaq.us-east-1.rds.amazonaws.com;Database=Minecraft;User Id=admin;Password=FlnJhLbAqs8Ij0VLtRCO;TrustServerCertificate=True;MultipleActiveResultSets=True;");
+
+
+        // GET api/<MapPointController>/5
+        [HttpGet("GetAllPoints")]
+        public async Task<List<MapPoint>> GetAllPoints()
+        {
+            List<MapPoint> points;
+            try
+            {
+                points = await mpDA.GetAllMapPointsAsync();
+            }
+            catch (Exception ex)
+            {
+                points = new List<MapPoint>();
+            }
+            return points;
+        }
+
+        // POST api/<MapPointController>
+        [HttpPost]
+        public async Task<string> Post([FromBody] MapPoint mapPoint)
+        {
+            try
+            {
+                await mpDA.AddMapPointAsync(mapPoint);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return mapPoint.Coordinates;
+        }
+    }
+}
---
+++ DA/MapPoint/MapPointDA.cs
@@ -0,0 +1,41 @@
+﻿namespace MinecraftTrackerApp.DA.MapPoint
+{
+    using Dapper;
+    using System.Threading.Tasks;
+    using MinecraftTrackerObjects;
+    using Microsoft.Data.SqlClient;
+
+    public class MapPointDA
+    {
+        private readonly string _connectionString;
+
+        public MapPointDA(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<List<MapPoint>> GetAllMapPointsAsync()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sql = "SELECT MapPointId, Name, Coordinates, Position FROM MapPoint";
+
+                // QueryAsync<T> is a Dapper method to run a query and map the result to a list of T
+                var mapPoints = await connection.QueryAsync<MapPoint>(sql);
+
+                // Convert the result to a List<MapPoint>
+                return mapPoints.AsList();
+            }
+        }
+
+        public async Task AddMapPointAsync(MapPoint mapPoint)
+        {
+            var sql = "INSERT INTO MapPoint (MapPointId, Name, Coordinates, Position) VALUES (NEWID(), @Name, @Coordinates, @Position)";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.ExecuteAsync(sql, new { mapPoint.Name, mapPoint.Coordinates, mapPoint.Position });
+            }
+        }
+    }
+}
